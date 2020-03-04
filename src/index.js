a380eadb73d9f6d485b59890461c83de4c587a49
ROnic404 <ronic@inbox.ru>
2020-03-04 21:43:23 +0400

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let strArray = str.split('');
+    if(strArray.length%2 !== 0) return false;
+    let openBrackets = [];
+    let closeBrackets = [];    
+    
+    for(let i of bracketsConfig) {
+        openBrackets.push(i[0]);
+        closeBrackets.push(i[1]);
+    }
+
+    for(let i = 0; i < strArray.length; i++) {
+        for(let j = 0; j < openBrackets.length; j++) {
+            if(strArray[i] === openBrackets[j] && strArray[i+1] === closeBrackets[j]) {
+                strArray.splice(i, 2);
+                i -= 2;
+            } 
+        }
+    }
+
+    return (strArray.length === 0) ? true : false;
 }
